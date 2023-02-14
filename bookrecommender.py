67a59c11305f7d91e5858688b4f8a612e69786a7
Sancerro <karaerbaris@gmail.com>
2023-02-14 11:30:48 +0300

    added functions to load data and suggest books

--- bookrecommender.py
+++ bookrecommender.py
@@ -1,4 +1,23 @@
 
 import pandas as pd
-df = pd.read_csv('Books.csv')
-print(df.to_string())
+
+def load_data():
+    books_df = pd.read_csv('Books.csv')
+    ratings_df = pd.read_csv('Ratings.csv')
+    books_df = pd.merge(books_df, ratings_df, on="ISBN")
+    return books_df
+
+def get_book_suggestions(books_df, prefix, type):
+    suggestions = []
+    if type == "author":
+        for author in books_df['Book-Author'].unique():
+         if author.startswith(prefix):
+            suggestions.append(author)
+    else:
+        for title in books_df['Book-Title'].unique():
+         if title.startswith(prefix):
+            suggestions.append(title)
+    
+    return suggestions
+
+
